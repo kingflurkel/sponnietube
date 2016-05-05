d8bbd8ebd214b13b9d22211419cdebc3e3268456
kingflurkel <mikethuy@gmail.com>
2016-05-05 22:17:01 +0200

    ..

--- app/contracts/upload.sol
+++ app/contracts/upload.sol
@@ -1,30 +1,88 @@
-/// @title Blocktu.be user contract.
+/// @title [TESTNET] Blocktu.be user contract.
 
-contract blocktubeUpload {
+/* 
+
+    Blocktube has two tokens:
+    a/ The BTToken
+    b/ The ClipToken
+
+    A blocktube clip has a limited supply of tokens (cliptoken).
+    The ClipTokens are created by depositing BTToken ('like').
     
-	// Here we create an array named 'clips'. 
-	// We will store ipfs hashes here.
-    string[] public clips;
-    address owner;
-    string public playlistname;
+    After a certain treshold is met (end of the minting / crowdsale), 
+    the next BTTokens (likes) will be distributed amongst 
+    the token holders, in relation to the amount 
+    of ClipTokens they hold.
 
-    function blockPlaylist(string playlist) { 
-    	owner = msg.sender; 
-    	playlistname = playlist;
+*/
+
+// we add the token contract so later, it knows what to do.
+contract token { function transfer(address receiver, uint amount){  } }
+
+contract blocktubeClip {
+    
+	// First, we need to declare some variables used in this contract.
+    // The address of the original poster
+    address public owner;
+
+    // The percentage of shares the original poster is willing to trade
+    uint public treshold;
+
+    // The address of the Blocktube token contract
+    uint public token;
+
+    // The total supply of clipshares
+    uint public clipshares;
+
+    // Every clip has a json object, that's stored on IPFS.
+    string public ipfsclipobject;
+    
+    // We have an array of 'funders', the early likes.
+    Shareholder[] public shareholders;
+    
+    // The shareholder object
+    struct Shareholder {
+        address addr;
+        uint shares;
     }
 
-    // Fire the event clipUploaded(clipobject)
-    // so our bot/gui can listen to it
-    event clipUploaded(string ipfshash);
+    // We have a boolean, crowdsaleClosed
+    bool tresholdReached = false;
 
-    // Add the ipfshash to the clipcollection
-    function uploadClip(string ipfshash){
-    	// If the upload is the owner of this playlist
-    	if (msg.sender != owner)
-    		throw;	
-    	// push into clips array
-    	clips.push(ipfshash);
-    	// fire an event we can listen to
-    	clipUploaded(ipfshash);
+    // And of course, the array with likes / shares
+    mapping(address => uint256) public balanceOf;
+
+    // Then we define some events, where our contraclistener can listen to.
+    //event tresholdReached(likesBalance);
+
+    // And now, the function that runs when deploying this contract.
+    function blocktubeClip(string _ipfsclipobject, uint _treshold, uint _clipshares){
+        owner = msg.sender;
+        treshold = _treshold;
+        clipshares = _clipshares;
+        ipfsclipobject = _ipfsclipobject;
+        tresholdReached = false;
     }
+
+    // When someone sends a like token to this contract's balance in the token contract,
+    // the tokencontract will call this function
+    function blocktubeTransfer(address _liker, uint _likeamount){
+        // When the current amount of shareholders is lower than the treshold,
+        // add the msg.sender to shareholders, and give him the amount of
+        // shares left / number of shareholders.
+        if(shareholders.length < treshold){
+            uint shares = remainingCliptokens / shareholders.length;
+            shareholders[shareholders.length++] = Shareholder[{addr: msg.sender, shares: shares}];
+            remainingCliptokens = remainingCliptokens - amount;
+        } else {
+            // When we have reached the treshold, the likeamount is spread over the shareholders.
+            // We invoke the token contract's function 'transfer'
+            for (var i = shareholders.length - 1; i >= 0; i--) {
+                token.tranfer(_liker, _likeamount / shareholders[i].shares); 
+            }
+        }
+    }
+
+
+
 }
